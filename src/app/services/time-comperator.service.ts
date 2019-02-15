0944f55858df0a0609ccdc216febb084bb74b62a
Marta Slomiak <marta.slomiak@capgemini.com>
2019-02-15 17:42:29 +0100

    time comperator service functions

--- src/app/services/time-comperator.service.ts
+++ src/app/services/time-comperator.service.ts
@@ -6,4 +6,21 @@ import { Injectable } from '@angular/core';
 export class TimeComperatorService {
 
   constructor() { }
+
+  isStartBeforeCurrentDate(currentDate: Date, startDate: Date) {
+    return startDate.getTime() < currentDate.getTime(); 
+  }
+
+  isStopBeforeCurrentDate(currentDate: Date, stopDate: Date) {
+    stopDate.getTime() < currentDate.getTime(); 
+  }
+
+  isStartAfterCurrentDate(currentDate: Date, startDate: Date) {
+    return currentDate.getTime() > startDate.getTime()
+  } 
+  isStopAfterCurrentDate(currentDate: Date, stopDate: Date) {
+    return currentDate.getTime() > stopDate.getTime()
+  }
+
+
 }
